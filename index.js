58324aaa344c1b5f6a7bf4ae1fa3294d9eaba267
FaithOgendi <faithbrianna01@gmail.com>
2024-01-15 22:31:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement)
+        return true;
+    }
+  }
+  return false;
 }
-
-/* 
+/*
   Write the Big O time complexity of your function here
+hasTargetSum = O(n^2)
+n = length.array
+ outer loop
+ inner loop
+ result
 */
 
-/* 
+/*
   Add your pseudocode here
 */
 
 /*
   Add written explanation of your solution here
+  Outer loop:
+   (for (let i = 0; i < array.length; i++)) - iterates over each element
+   complement is calculated =  (const complement = target - array[i])
+  Inner Loop:
+     (for (let j = i + 1; j < array.length; j++))  - iterates over elements to the right of current element
+     if (array[j] === complement) - checks if current element is equal to the complement
+     returns true
+  No pair after both loops , returns false.
 */
 
 // You can run `node index.js` to view these console logs
